ae15944ccf1e56ffc1d04f2d8e451a9c15249b63
stewarhr <stewarhr@rose-hulman.edu>
2022-04-18 17:47:25 +0000

    max done

--- homeworks/13_condvar_basics/max.c
+++ homeworks/13_condvar_basics/max.c
@@ -2,15 +2,27 @@
 #include <pthread.h>
 #include <unistd.h>
 
+int max = 0;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t condmax = PTHREAD_COND_INITIALIZER;
+
 void *thread(void *arg)
 {
 	char *letter = (char *)arg;
+	
+	
 	printf("%c wants to enter the critical section\n", *letter);
-
+    pthread_mutex_lock(&lock);
+	while(max>= 3){
+		pthread_cond_wait(&condmax,&lock);
+	}
+	pthread_mutex_unlock(&lock);
+	max++;
 	printf("%c is in the critical section\n", *letter);
 	sleep(1);
 	printf("%c has left the critical section\n", *letter);
-
+	pthread_cond_signal(&condmax);
+    max--;
 	return NULL;
 }
 

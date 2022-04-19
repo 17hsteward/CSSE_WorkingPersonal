647463fe60ef234d7134f1a5ebff14aa69371ede
stewarhr <stewarhr@rose-hulman.edu>
2022-04-19 02:04:34 +0000

    inorder done

--- homeworks/13_condvar_basics/inorder.c
+++ homeworks/13_condvar_basics/inorder.c
@@ -2,13 +2,53 @@
 #include <pthread.h>
 #include <unistd.h>
 
+int onedone = 0;
+int twodone = 0;
+int threedone = 0;
+pthread_cond_t conO = PTHREAD_COND_INITIALIZER;
+pthread_cond_t conT = PTHREAD_COND_INITIALIZER;
+pthread_cond_t conH = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
 void *thread(void *arg)
 {
         int *num = (int *)arg;
+        
         printf("%d wants to enter the critical section\n", *num);
-
+        if(*num != 1){
+           pthread_mutex_lock(&lock);
+           while(onedone == 0)
+                pthread_cond_wait(&conO,&lock);
+           pthread_mutex_unlock(&lock);
+        }
+        if(*num != 1 && *num != 2){
+           pthread_mutex_lock(&lock);
+           while(twodone == 0 || onedone == 0)
+                pthread_cond_wait(&conT,&lock);
+           pthread_mutex_unlock(&lock);
+        }
+        if(*num != 1 && *num != 2 && *num != 3){
+           pthread_mutex_lock(&lock);
+           while(twodone == 0 || onedone == 0 || threedone == 0)
+                pthread_cond_wait(&conH,&lock);
+           pthread_mutex_unlock(&lock);
+        }
+        pthread_mutex_lock(&lock);
         printf("%d is finished with the critical section\n", *num);
-
+        
+        if(*num == 1){
+           onedone = 1;
+           pthread_cond_signal(&conO);
+        }
+        if(*num == 2){
+           twodone = 1;
+           pthread_cond_signal(&conT);
+        }
+        if(*num == 3){
+           threedone = 1;
+           pthread_cond_signal(&conH);
+        }
+        pthread_mutex_unlock(&lock);
         return NULL;
 }
 
@@ -22,7 +62,7 @@ main(int argc, char **argv)
         for(i = 0; i < 4; ++i) {
                 pthread_create(&threads[i], NULL, thread, &nums[i]);
 
-                if(i == 2)
+                 if(i == 2)
                         sleep(3);
         }
 
